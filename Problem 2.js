83221c0fa2cda6563bc2d8630fd7f6f0a593688a
SeanCox <sean.cox@mac.com>
2016-02-16 00:28:32 -0800

    work on problem 2

--- Problem 2.js
+++ Problem 2.js
@@ -1,5 +1,14 @@
-var fib = 0,
-    counter = 0;
+var fib = 1,
+    total = 0;
+
+var fibonacci = function (fib){
+
+  return
+
+}
+
+
+
 
 var evenFibonacci = function(num){
   if (fib > num) return fib;
@@ -7,3 +16,20 @@ var evenFibonacci = function(num){
 
 
 }
+
+
+
+// evenfib takes in a number
+
+//checks to see if if fib is greater than number
+  //if it is it returns total
+
+// checks to see if number is even
+  //if it is it adds it to total
+
+//runs fib to get next number in sequence
+
+//call evenFib
+
+
+// fib returns a sum of even fibs(total)
