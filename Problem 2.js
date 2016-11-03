44ab4af1798e985d5eea5aaf6c0cbdae02e62bfa
SeanCox <sean.cox@mac.com>
2016-11-02 19:11:16 -0700

    [solved] problem 2

--- Problem 2.js
+++ Problem 2.js
@@ -1,35 +1,14 @@
-var fib = 1,
-    total = 0;
+let fib = [1,2]
+let answer = 0
 
-var fibonacci = function (fib){
-
-  return
-
+while(fib[fib.length-1] <= 4000000){
+  fib.push(fib[fib.length-1] + fib[fib.length-2])
 }
 
+answer = fib.reduce((acc,cur)=>{
+  if(cur % 2 === 0) return acc + cur
+  return acc
+},0)
 
-
-
-var evenFibonacci = function(num){
-  if (fib > num) return fib;
-
-
-
-}
-
-
-
-// evenfib takes in a number
-
-//checks to see if if fib is greater than number
-  //if it is it returns total
-
-// checks to see if number is even
-  //if it is it adds it to total
-
-//runs fib to get next number in sequence
-
-//call evenFib
-
-
-// fib returns a sum of even fibs(total)
+console.log(answer)
+return answer
